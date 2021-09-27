fdd5e7e1970b27c337bb3358c60ccb6dae57f025
kelley-mcd <kmcdonnell8@gmail.com>
2021-09-27 18:02:54 -0400

    commit

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 
+
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll();
