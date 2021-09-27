e38c0d65bcfa3a14156f147ff1e53ccf16a7abe1
kelley-mcd <kmcdonnell8@gmail.com>
2021-09-27 17:53:11 -0400

    commit

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -37,7 +37,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async, (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const categoryData = await Category.update(req.body);
     res.status(200).json(categoryData);
